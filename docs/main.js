b5125b41fc11cafd1138c2fddb6bd34c52240776
Oleksandr Zaitsev <olk.zaytsev@gmail.com>
2021-02-10 04:07:22 +0100

    Adding ideas.json

---
+++ docs/main.js
@@ -0,0 +1,17 @@
+"use strict";
+
+const ideasUrl = 'https://raw.githubusercontent.com/pharo-project/pharo-project-proposals/master/Topics.st';
+
+function getContentsOfFileFromURL(url) {
+  var result = null;
+  $.ajax({
+    url: url,
+    type: 'get',
+    dataType: 'text',
+    async: false,
+    success: function(data) {
+      result = data;
+    }
+  });
+  return result;
+}
