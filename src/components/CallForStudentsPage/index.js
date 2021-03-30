c5b6eac949f6396ef19925eb2e9c71ac6cbe357a
Oleksandr Zaitsev <olk.zaytsev@gmail.com>
2021-03-30 13:27:12 +0200

    A pass on the Call for Students

--- src/components/CallForStudentsPage/index.js
+++ src/components/CallForStudentsPage/index.js
@@ -1,7 +1,78 @@
 import React from 'react';
 
 const CallForStudentsPage = () => (
-  <h1>Call for Students</h1>
+<div>
+<h1>Google Summer of Code 2021: Call for Students</h1>
+
+<p>Dear students,</p>
+
+<p>We are happy to announce that once again, Pharo Consortium is looking for smart and enthusiastic students to work with us during Google Summer of Code 2021. In the following sections, we explain the program requirements and provide a detailed description of the application process. This text is based on the <a href="https://summerofcode.withgoogle.com/rules/">Google Summer of Code 2021 Program Rules</a>. We strongly suggest that you read the original rules. In case of any disagreement between this document and the Rules, the Rules should prevail.</p>
+
+<p>If you have any additional questions, feel free to send an email to any of the organizers listed at the bottom of this page.</p>
+
+<h2>1. What is GSoC?</h2>
+
+<p>Google Summer of Code (GSoC) is a global program focused on introducing students to open source software development. Students work on a 2-month programming project with an open-source organization during their break from university. You can read more about the program on its <a href="https://summerofcode.withgoogle.com/">official website</a>. We want to emphasize the following:</p>
+
+<ul>
+  <li>The program's active part starts on May 17 and ends on August 31, when the results are announced.</li>
+  <li>The first month of the program (May 17 - June 7) is called the community bonding period. During this time, you will be introduced to our community, get comfortable with our communication channels (Discord and mailing lists), get to know your mentors, prepare a detailed timeline for the summer, etc.</li>
+  <li>The coding phase begins on June 7 and ends on August 16. During this time, you will be working on your projects, regularly communicating with your mentors, writing blog posts, etc.</li>
+  <li>During its active part, GSoC should be treated as a full-time job. If you have more than a few hours a week of extra commitments, you probably should skip GSoC; it is unlikely that you will be successful. In any case, be completely clear about outside time commitments as part of your proposal.</li>
+  <li>Accepted students will be receiving a stipend (payment) from Google at the end of each month. <a href="https://developers.google.com/open-source/gsoc/help/student-stipends">Stipend amounts</a> are calculated based on your location.</li>
+</ul>
+
+<h2>2. What is Pharo?</h2>
+
+<p>Pharo is a dynamic, purely object-oriented programming language (everything is an object) in the tradition of Smalltalk. But it is also a robust development environment, focused on simplicity and immediate feedback. Its full syntax fits on a postcard, and coding can be done directly in the debugger. Pharo has super cool tools that empower you and make you highly efficient.</p>
+
+<p>Pharo's goal is to deliver a clean, innovative, free and open-source immersive environment. By providing a stable and small core system, excellent development tools, and maintained releases, Pharo is an attractive platform to build and deploy mission-critical applications.</p>
+
+<p>Pharo fosters a healthy ecosystem developed from private and commercial contributors who advance and maintain its core system and external packages.
+  More information about Pharo is available at <a href="http://www.pharo.org/">http://www.pharo.org/</a>.</p>
+
+<h2>3. Eligibility criteria</h2>
+
+<p>To apply for Google Summer of Code with Pharo Consortium, you must satisfy the Eligibility Criteria for students found in the corresponding section of the <a href="https://summerofcode.withgoogle.com/rules/">Program Rules</a>.</p>
+
+<p>We want to emphasize that every applicant must be:</p>
+
+<ul>
+  <li><b>eighteen (18) years of age or older</b> upon registration for the program;</li>
+  <li><b>enrolled into a post-secondary academic program</b>, as of the acceptance date or have graduated from the program between December 1, 2020, and acceptance date or has been accepted into a post-secondary academic program that begins by October 1, 2021;</li>
+  <li>eligible to work in the country in which they reside for the duration of the program;</li>
+  <li>not an Organization Administrator or Mentor in the program.</li>
+</ul>
+
+<h3>3.1. Strongly preferred by Pharo Consortium</h3>
+
+<ul>
+  <li>Good knowledge of OOP</li>
+  <li>Basic idea about Pharo & Smalltalk syntax and ongoing projects</li>
+  <li>Experience with Pharo & Smalltalk</li>
+</ul>
+
+<h2>4. Before applying (how to increase your chances of being accepted)</h2>
+
+<ul>
+  <li>Check the <a href="https://gsoc.pharo.org/ideas">list of project ideas</a>. Select one or two projects that you find interesting and think you have the right set of skills. Don’t be afraid to apply to projects where you only meet 51% of the listed requirements. Include a section on how you’ll compensate for or learn the missing skills — and demonstrate during the application process that you are working on acquiring those new skills.</li>
+  <li>Join the Pharo Discord channel (follow the <a href="https://pharo.org/community">instructions</a>), go to the #gsoc-students channel and introduce yourself. Write who you are, where you come from, your interests, and which projects you would like to work on. Talk to other students, ask them questions, and get to know them. If you have trouble joining Discord, feel free to send a letter to any of the program organizers whose email addresses are listed at the bottom of this page.</li>
+  <li>Get in touch with the mentors assigned to the projects you are interested in by email or Discord. You can find the email addresses of all our mentors in the <a href="https://gsoc.pharo.org/ideas">list of project ideas</a>. Talk to them about the complexity and skills required for the different projects.</li>
+  <li>During the selection process, we will pay special attention to your experience with OOP and knowledge of Pharo. If you are new to Pharo, use the time you have before selection to enrich your portfolio. Read several chapters of <a href="https://books.pharo.org/updated-pharo-by-example/">Pharo by Example book</a> or take the <a href="https://mooc.pharo.org/">Pharo MOOC</a> - videos, exercises, challenges, and mini-projects. But most importantly, write something cool in Pharo and put it on your GitHub. It can be a simple project or exercise. If we see that you can write code and do it well, this will increase your chances of being accepted.</li>
+  <li>If you have a personal technical blog, this will also increase your chances. If you don’t have it, now is an excellent time to start. Write about the things you learn or the projects you work on. For example, you can write about your experience learning Pharo and describe some simple project you implemented (such as linear regression, tic-tac-toe game, or linked list).</li>
+  <li>Interact with Pharo Community. Ask and answer questions on Discord, join our <a href="https://pharo.org/community">mailing lists</a>. Participate in our <a href="https://pharo.org/contribute-events">monthly sprints</a>. Try fixing some bugs, open some relevant issues, suggest changes or additional features (see Pharo's <a href="https://github.com/pharo-project/pharo/issues">issue tracker</a>). The better the contributions, the better are the chances of selection.</li>
+  <li><b>IMPORTANT:</b> Start working on your project proposal (see the section on how to apply) as soon as possible. The proposal is an extensive and detailed document that has to be carefully and diligently prepared. So contact the mentors right away and start working on your proposal.</li>
+</ul>
+
+<p><b>HINT:</b> If you have a project idea that is not on the Idea List, negotiate it with the community, it may be of interest, and mentors might be found.</p>
+
+<h3>4.1. Things to remember</h3>
+
+<p>Although we pay great attention to your technical skills, <b>communication skills are way more important</b>. We must see that you can work remotely, be active on forums, not be afraid to ask questions or express your opinion. Students who are active in our communication channels have a higher chance of being selected.</p>
+
+<p>Be proactive - <b>do not wait to be told what to do</b>. Your mentors are here to answer your questions, help you get unstuck, make sure that you are on track. But they will not give you step-by-step instructions or make decisions for you. Show us that you can take the initiative in your own hands and work independently even when your mentors are not there to help you (although we will try to make sure that they always are). Show us that you have been working on some projects outside the classroom, especially projects that you did "for fun".</p>
+
+</div>
 );
 
 export default CallForStudentsPage;
