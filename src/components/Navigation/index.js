f388ea0416357e1c41ef58ffe313d9ebca4e75f2
Oleksandr Zaitsev <olk.zaytsev@gmail.com>
2021-03-31 00:38:42 +0200

    Added a Pharo logo

--- src/components/Navigation/index.js
+++ src/components/Navigation/index.js
@@ -4,15 +4,20 @@ import { Link } from 'react-router-dom';
 import './style.css';
 import * as ROUTES from '../../constants/routes';
 
+import logo from '../../img/logo-small.png';
+
 const Navigation = () => (
   <div id="navbar">
     <div id="navbar-buttons">
       <ul>
         <li>
-          <Link to={ROUTES.LANDING}>Home</Link>
+          <Link to={ROUTES.LANDING}><img src={logo} alt='' /></Link>
         </li>
         <li>
-          <Link to={ROUTES.IDEAS}>Project Ideas</Link>
+          <Link class="navbar-button" to={ROUTES.LANDING}>Home</Link>
+        </li>
+        <li>
+          <Link class="navbar-button" to={ROUTES.IDEAS}>Project Ideas</Link>
         </li>
       </ul>
     </div>
