21d928d054be010c6771f9629d9eaedda89d06cc
Oleksandr Zaitsev <olk.zaytsev@gmail.com>
2023-02-07 23:55:43 +0100

    Fixed the links

--- website/src/components/IdeasPage/IdeaCard.js
+++ website/src/components/IdeasPage/IdeaCard.js
@@ -19,35 +19,24 @@ const shorten = (text, maxlength) => {
   return shortened + " (...)";
 }
 
-export default function IdeaCard({
-  idea,
-  children,
-}) {
+export default function IdeaCard({idea}) {
+
   const { id, title, description, img, goal, url, supervisors, skills, size, difficulty } = idea;
   const link = id;
 
   return (
-    <div id={title.replace(/\W/g, '_')} className={"ideaCard"}>
-      <div className={"header"}>
-        {img &&
-          <div className={"projectLogo"}>
-            <a href={link}>
-              <img
-                src={img ? URLS.IMAGE_FOLDER + '/' + img : logo}
-                alt={title}
-                className={"projectLogo"}
-              />
-            </a>
-          </div>
-        }
-        <div className={"projectName"}>
-          <a href={link}>
-            {title}
-          </a>
+    <a href={link} id={title.replace(/\W/g, '_')} className="ideaCard">
+      <div className="header">
+        <div className="projectLogo">
+          <img
+            src={img ? URLS.IMAGE_FOLDER + '/' + img : logo}
+            alt={title}
+            className="projectLogo"
+          />
         </div>
+        <div className="projectName">{title}</div>
       </div>
-      <p className={"projectDescription"}>{shorten(description, 100)}</p>
-      <div className={"projectDescription"}>{children}</div>
-    </div>
+      <p className="projectDescription">{shorten(description, 100)}</p>
+    </a>
   )
 }
