5efa1ef03c61f0a40eaa7708dd9c334573333ae1
Oleksandr Zaitsev <olk.zaytsev@gmail.com>
2023-02-08 11:55:03 +0100

    Moved data a level higher

--- website/src/constants/urls.js
+++ website/src/constants/urls.js
@@ -2,7 +2,7 @@ const REPO = 'https://raw.githubusercontent.com/pharo-project/pharo-gsoc/'
 
 const BRANCH = 'master'
 
-export const BASE = REPO + '/' + BRANCH + '/website/data'
+export const BASE = REPO + '/' + BRANCH + '/data'
 
 export const IDEAS_FILE = BASE + '/ideas.json';
 export const SUPERVISORS_FILE = BASE + '/supervisors.json';
